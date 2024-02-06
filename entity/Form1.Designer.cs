2fdc6cd34a553a1986e4c21a10ec136781884bf4
WhiteFishDen <muslinazavtrak@gmail.com>
2024-02-06 21:05:35 +0300

    Добавьте файлы проекта.

---
+++ entity/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace entity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(256, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Fill";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 148);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(105, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(105, 222);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(256, 148);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(520, 290);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ entity/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using Npgsql;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace entity
+{
+    public partial class Form1: Form
+    {
+        public class LibraryContext<T> : DbContext where T: class
+        {
+            private static string connString = "Host=localhost;Username=postgres;Password=123;Database=MusicShop;";
+            
+            public LibraryContext() : base(new NpgsqlConnection(connString), true) {  }
+            public DbSet<T> product { get; set; } 
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new string[] { "Music Record", "Performer", "Record Studio" });
+            
+            var db = new LibraryContext<MusicRecord>();
+            db.product.Load();
+            dataGridView1.DataSource = db.product.Local.ToBindingList();
+            this.dataGridView1.Columns["Performer"].Visible = false;
+            this.dataGridView1.Columns["RecordStudio"].Visible = false;
+
+        }
+        private void LoadTable()
+        {
+            var db = new LibraryContext<MusicRecord>();
+            db.product.Load();
+            dataGridView1.DataSource = db.product.Local.ToBindingList();
+            this.dataGridView1.Columns["Performer"].Visible = false;
+            this.dataGridView1.Columns["RecordStudio"].Visible = false;
+        }
+        
+
+        [Table("record_studio", Schema = "public")]
+        public class RecordStudio
+        {
+            [Column("id")] public int? Id { get; set; }
+            [Column("name")] public string Name { get; set; }
+            [Column("adress")] public string Adress { get; set; }
+
+        }
+
+        [Table ("performer", Schema = "public")]
+        public class Performer
+        {
+            [Column("id")] public int? Id { get; set; }
+            [Column("name")] public string Name { get; set; }
+            [Column("year_of_foundation")] public DateTime? Year_of_foundation { get; set; }
+        }
+
+
+
+        [Table("music_record", Schema = "public")]
+        public class MusicRecord
+        {
+            [Column("id")] public int? Id { get; set; }
+            [Column("name")] public string Name { get; set; }
+            [Column("year")] public DateTime? Date { get; set; }
+            [Column("count_of_songs")] public int? Count_of_songs { get; set; }
+            [Column("genre")] public string Genre { get; set; }
+            [Column ("price")] public double? Price { get; set; }
+
+            [Column("id_performer")] public int? PerformerId { get; set; }
+            [Column("id_record_studio")] public int?  RecordStudioId { get; set; }
+
+           public virtual  Performer Performer { get; set; }
+           public virtual RecordStudio RecordStudio { get; set; }
+
+        }
+          
+
+
+            void GetAllRecords()
+            {
+               using (LibraryContext<MusicRecord> context = new LibraryContext<MusicRecord>())
+               {
+                    var table = context.product.ToList();
+                    foreach (var entity in table)
+                    {
+                    Invoke(new Action(() =>
+                    { comboBox1.Items.Add(entity.Name + " " + entity.RecordStudio.Name + " " + entity.Performer.Name); })); 
+                    }
+               }
+            }
+
+        static void AddProduct(MusicRecord  prod)
+        {
+            using (LibraryContext<MusicRecord> ctx = new LibraryContext<MusicRecord>())
+            {
+                MusicRecord  a = ctx.product.Where((x) => x.Id == prod.Id).FirstOrDefault();
+                if (a == null)
+                {
+                    ctx.product.Add(prod);
+                    ctx.SaveChanges();
+                }
+            }
+        }
+        async Task PrintAsync()
+        {
+            await Task.Run(() => GetAllRecords());
+        }
+
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await PrintAsync();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0) {  }
+        }
+
+
+
+        //private async void button2_Click(object sender, EventArgs e)
+        //{
+        //    MusicRecord newRecord = new MusicRecord { Id = Convert.ToUInt16(textBox1.Text), 
+        //                                              Name = textBox2.Text,
+        //                                              Date = Convert.ToDateTime(textBox3.Text),
+        //                                              Count_of_songs = };
+        //}
+    }
+}
+    
+
